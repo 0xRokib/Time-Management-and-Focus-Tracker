7af7e199b979a5ecc22204160285a3f0a8f302d3
rokib97 <rokibulhasan.ph@gmail.com>
2025-01-06 18:31:08 +0600

    added rate limit middleware

--- focus-tracker-backend/src/modules/focus/focus.route.ts
+++ focus-tracker-backend/src/modules/focus/focus.route.ts
@@ -1,5 +1,6 @@
 import { RequestHandler, Router } from "express";
 import { authMiddleware } from "../../middlewares/authMiddleware";
+import { rateLimiter } from "../../middlewares/rateLimitMiddleware";
 import { logFocusSessionHandler } from "./focus.controller";
 
 const focusRouter = Router();
@@ -7,6 +8,7 @@ const focusRouter = Router();
 focusRouter.post(
   "/focus-session",
   authMiddleware as RequestHandler,
+  rateLimiter("focus:log") as RequestHandler,
   logFocusSessionHandler as RequestHandler
 );
 
