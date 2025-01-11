90f3b4c2538787a5153ad2e083c9209ac2050a4f
rokib97 <rokibulhasan.ph@gmail.com>
2025-01-11 09:23:01 +0600

    update : added redis env to host in render

--- focus-tracker-backend/src/config/dotenvConfig.ts
+++ focus-tracker-backend/src/config/dotenvConfig.ts
@@ -1,18 +1,25 @@
 const dotenv = require("dotenv");
 
+// Load environment variables
 dotenv.config();
 
-module.exports = {
+const config = {
   PORT: process.env.PORT || 5001,
-  JWT_SECRET: process.env.JWT_SECRET,
+  JWT_SECRET: process.env.JWT_SECRET || "default_jwt_secret",
   DB_CONFIG: {
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
     password: process.env.DB_PASS,
-    port: process.env.DB_PORT,
+    port: process.env.DB_PORT || 5432,
   },
   REDIS_CONFIG: {
-    url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
+    url:
+      process.env.REDIS_URL ||
+      `redis://${process.env.REDIS_HOST || "localhost"}:${
+        process.env.REDIS_PORT || 6379
+      }`,
   },
 };
+
+module.exports = config;
